8971369ed232c1b8728ef4d92e08827099fe96a4
DragonNimble <48077655+DragonNimble@users.noreply.github.com>
2022-05-19 23:03:22 +0800

    Update Show520.c

--- Show520.c
+++ Show520.c
@@ -1,9 +1,7 @@
 #include <REGX52.H>
-#include <INTRINS.H>
-
 ////////////////////
 //This program will show 1314520 
-/////////////////
+///////////////////
 
 void Delay(unsigned int xms)		//@12.000MHz
 {
@@ -20,8 +18,8 @@ void Delay(unsigned int xms)		//@12.000MHz
 }
 void main()
 {
-	int a = 7;
-	int b = 0;
+	unsigned char  a = 7;
+	unsigned char  b = 0;
 	unsigned char Show[7] = {0X06,0X4F,0X06,0X66,0X6d,0X5b,0X3F};
 	while(1)
 	{
@@ -29,10 +27,10 @@ void main()
 		P2_3 = (a / 2) % 2;
 		P2_2 = a % 2;
 		a -= 1;
-		P0 = Show[b];
-		b = (b + 1) % 7;
 		if(a == 0)
 			a = 7;
+		P0 = Show[b];
+		b = (b + 1) % 7;	
 		Delay(500);
 	}
 }
